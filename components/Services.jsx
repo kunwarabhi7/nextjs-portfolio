fe159ff954731808468d7f9f6c65f953347371ec
Abhishek <abhishekparmar162@gmail.com>
2022-10-21 23:13:14 +0530

    completed services writing

--- components/Services.jsx
+++ components/Services.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Services = () => {
   return (
     <div>
-        <h2>Services i Offer</h2>
+      <h2 className="text-4xl text-center pt-4 font-bold text-green-600">
+        Services i Offer
+      </h2>
+      <p className="text-md py-2 leading-8 text-gray-800">
+        Since the beganining of my journey as a freelancer devloper i workded
+        for <span className="text-green-600"> agencies </span>
+        consulted for <span className="text-green-600"> StartUP </span> and collebrated with talented people to create digital product for both buisness and consumer use.
+      </p>
+      <p  className="text-md py-2 leading-8 text-gray-800 ">     I offer from a wide range of services, including brand design,
+              programming and teaching.</p>
     </div>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
