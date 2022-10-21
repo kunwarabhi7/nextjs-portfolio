0054b2aa53b8069e29459a6d3e8b8f4c57ced00f
Abhishek <abhishekparmar162@gmail.com>
2022-10-21 22:36:18 +0530

    home page setup

--- components/HomeScreen.jsx
+++ components/HomeScreen.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import {FaTwitter , FaLinkedin ,FaDiscord }  from 'react-icons/fa'
 
 const HomeScreen = () => {
   return (
-    <div>
-      <h1>Abhishek Singh</h1>
-      <h2>Front End Web Dev</h2>
-      <p>Build app using nextJS reactJS tailwind CSS, A freelancer and a gamer who codes . Also a passionate gymer</p>
+    <div className='text-center p-4'>
+      <h1 className='text-4xl font-bold text-green-500'>Abhishek Singh</h1>
+      <h2 className='font-bold py-3 '>Front End Web Dev</h2>
+      <p className='text-gray-700'>Build app using nextJS reactJS tailwind CSS, A freelancer and a gamer who codes . Also a passionate gymer</p>
+    <div className='flex justify-center pt-4 '>
+<FaTwitter className='text-blue-600 m-2' size={30} />
+<FaLinkedin className='text-gray-600 m-2' size={30}/>
+<FaDiscord  className='text-red-700 m-2' size={30}/>
+    </div>
     </div>
   )
 }
