2c10318cf73f25288965d7d2ba0c1b2622aa3b74
Abhishek <abhishekparmar162@gmail.com>
2022-10-22 17:49:10 +0530

    added links sidebar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -55,40 +55,42 @@ const NavBar = ({darkmode , setDarkmode}) => {
 <CgMenuGridR className='text-red-500' size={50} />
         </div>
         </div>
-        <div className='fixed top-0 left-0 md:hidden w-full h-screen bg-black/70'>
+        <div className='fixed top-0 left-0  w-full h-screen bg-black/70'>
 <div className='fixed top-0 left-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-gray-900'>
 <div>
-  <div className='flex justify-between p-4'>
-    <Image src={Logo} width='196' height='40' />
-    <GiCrossedSwords size={40} />
+  <div className='flex justify-between text-white p-4'>
+    <Image src={Logo} width='196' height='72' />
+    <GiCrossedSwords  size={40} />
 
   </div>
-  <ul>
+  <p className='text-green-300 text-center font-bold text-3xl   p-8'>Front End Web Dev</p>
+  <ul className='pt-8'>
   <Link href='/'>
-              <li className=' px-3 hover:underline font-bold dark:text-white'>
+              <li className=' px-3 hover:underline text-4xl font-bold py-4 text-white dark:text-black'>
                 Home
               </li>
             </Link>
             <Link href='/'>
-              <li className='px-3  hover:underline font-bold dark:text-white'>
+              <li className=' px-3 hover:underline text-4xl font-bold py-4 text-white dark:text-black'>
                 Skills
               </li>
             </Link>
             <Link href='/'>
-              <li className='px-3 hover:underline font-bold dark:text-white'>
+              <li className=' px-3 hover:underline  text-4xl font-bold py-4 text-white dark:text-black'>
                 Projects
               </li>
             </Link>
             <Link href='/'>
-              <li className='px-3 hover:underline font-bold dark:text-white'>
+              <li className=' px-3 hover:underline text-4xl font-bold py-4 text-white dark:text-black'>
                 Experience
               </li>
             </Link>
             <Link href='/'>
-              <li className='px-3 hover:underline font-bold dark:text-white'>
+              <li className=' px-3 hover:underline text-4xl font-bold py-4 text-white dark:text-black'>
                 Contact
               </li>
             </Link>
+            
   </ul>
 </div>
 </div>
