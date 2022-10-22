afa67bb212f0b46cef43d4749e31d855271d0531
Abhishek <abhishekparmar162@gmail.com>
2022-10-22 16:32:12 +0530

    added nav bar hover effect

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,16 +1,43 @@
 import {BsMoonFill} from 'react-icons/bs'
 import Logo from '../public/assets/logo.png'
 import Image from 'next/image'
+import Link from 'next/link'
 const NavBar = ({darkmode , setDarkmode}) => {
   return (
-    <div className='p-2 dark:bg-gray-900'>
+    <div className='p-2 dark:bg-gray-900 '>
         <ul className='flex  justify-between '>
-            <li className='w-64 h-20  dark:text-white'>
-              <Image className='cursor-pointer' src={Logo}/>
-            </li>
-            <div className='flex my-4 px-4 '>
+            <Link href='/'>
+              <li className='w-64 h-20  dark:text-white'>
+                <Image className='cursor-pointer' src={Logo}/>
+              </li>
+            </Link>
+            <div className='flex my-4 px-3 justify-evenly'>
 
-            <li className='bg-cyan-500 px-2 py-2 rounded-md text-white  pr-4 mr-2 cursor-pointer '>Resume</li>
+            <Link href='/'>
+              <li className=' px-3 hover:underline font-bold dark:text-white'>
+                Home
+              </li>
+            </Link>
+            <Link href='/'>
+              <li className='px-3  hover:underline font-bold dark:text-white'>
+                Skills
+              </li>
+            </Link>
+            <Link href='/'>
+              <li className='px-3 hover:underline font-bold dark:text-white'>
+                Projects
+              </li>
+            </Link>
+            <Link href='/'>
+              <li className='px-3 hover:underline font-bold dark:text-white'>
+                Experience
+              </li>
+            </Link>
+            <Link href='/'>
+              <li className='px-3 hover:underline font-bold dark:text-white'>
+                Contact
+              </li>
+            </Link>
             <li className=' cursor-pointer dark:text-white'><BsMoonFill onClick={()=> setDarkmode(!darkmode)} size={30}/></li>
             </div>
         </ul>
