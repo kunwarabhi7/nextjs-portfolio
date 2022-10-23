3ba43fe89c90c5ca284e556be8fa6a3d5539792e
Abhishek <abhishekparmar162@gmail.com>
2022-10-23 16:31:22 +0530

    fix sidebar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -8,129 +8,196 @@ import {CgMenuGridR} from 'react-icons/cg'
 import {GiCrossedSwords} from 'react-icons/gi' 
 
 
-
 const NavBar = ({darkmode , setDarkmode}) => {
-  const [sideBar ,setSideBar] = useState(false)
+   const [nav, setNav] = useState(false);
+  const [shadow, setShadow] = useState(false);
+  const [navBg, setNavBg] = useState('#ecf0f3');
+  const [linkColor, setLinkColor] = useState('#1f2937');
+  // const [position, setPosition] = useState('fixed')
+  // const router = useRouter();
 
-  const handleSideBar = () => {
-    setSideBar(!sideBar)
-  }
+  // useEffect(() => {
+  //   if (
+  //     router.asPath === '/property' ||
+  //     router.asPath === '/crypto' ||
+  //     router.asPath === '/netflix' ||
+  //     router.asPath === '/twitch'
+  //   ) {
+  //     setNavBg('transparent');
+  //     setLinkColor('#ecf0f3');
+  //   } else {
+  //     setNavBg('#ecf0f3');
+  //     setLinkColor('#1f2937');
+  //   }
+  // }, [router]);
 
+  const handleNav = () => {
+  
+    setNav(!nav);
+  };
+
+ 
   return (
-    <div className='p-0 md:p-2  dark:bg-gray-900 shadow-lg '>
-        <ul className='hidden fixed top-0 left-0 right-0 md:flex  justify-between '>
-            <Link href='/'>
-              <li className='w-64 h-20  dark:text-white'>
-                <Image className='cursor-pointer' src={Logo}/>
-              </li>
-            </Link>
-            <div className='flex my-4 px-3 justify-evenly'>
-
-            <Link href='/'>
-              <li className=' px-3 hover:underline font-bold dark:text-white'>
-                Home
-              </li>
-            </Link>
-            <Link href='/'>
-              <li className='px-3  hover:underline font-bold dark:text-white'>
-                Skills
-              </li>
-            </Link>
-            <Link href='/'>
-              <li className='px-3 hover:underline font-bold dark:text-white'>
-                Projects
-              </li>
-            </Link>
-            <Link href='/'>
-              <li className='px-3 hover:underline font-bold dark:text-white'>
-                Experience
-              </li>
-            </Link>
-            <Link href='/'>
-              <li className='px-3 hover:underline font-bold dark:text-white'>
-                Contact
-              </li>
-            </Link>
-            <li className=' cursor-pointer dark:text-white'><BsMoonFill onClick={()=> setDarkmode(!darkmode)} size={30}/></li>
-            </div>
-        </ul>
-
-        {/* Mobile View */}
-        <div className='md:hidden flex justify-between'>
+    <div
+      className='p-0 md:p-2  dark:bg-gray-900 shadow-lg '
+    >
+      <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16 dark:bg-gray-900'>
         <Link href='/'>
-              <li className='w-48 h-12  dark:text-white'>
-                <Image className='cursor-pointer' src={Logo}/>
-              </li>
-            </Link>
-        <div className='pt-4 pr-4 flex'>
-        <li className=' cursor-pointer dark:text-white'><BsMoonFill onClick={()=> setDarkmode(!darkmode)} size={30}/></li>
-<CgMenuGridR onClick={handleSideBar} className='text-red-500' size={50} />
+          <a>
+            <Image
+              src={Logo}
+              alt='/'
+              width='204'
+              height='90'
+              className='cursor-pointer w-64 h-20  dark:text-white'
+            />
+          </a>
+        </Link>
+        <div>
+          <ul style={{ color: `${linkColor}` }} className='hidden md:flex'>
+            <li className='px-3 hover:underline text-green-800 font-bold dark:text-white'>
+              <Link href='/'>Home</Link>
+            </li>
+            <li className='px-3 hover:underline text-green-800 font-bold dark:text-white'>
+              <Link href='/#about'>About</Link>
+            </li>
+            <li className='px-3 hover:underline font-bold text-green-800 dark:text-white'>
+              <Link href='/#skills'>Skills</Link>
+            </li>
+            <li className='px-3 hover:underline font-bold text-green-800 dark:text-white'>
+              <Link href='/#projects'>Projects</Link>
+            </li>
+           
+            <li className='px-3 hover:underline font-bold text-green-800 dark:text-white'>
+              <Link href='/#contact'>Contact</Link>
+            </li>
+            <li className=' cursor-pointer dark:text-white'><BsMoonFill onClick={()=> setDarkmode(!darkmode)} size={30}/></li>
+          </ul>
+          {/* Hamburger Icon */}
+          <div
+            className='md:hidden text-green-800 flex items-'
+          >
+            <CgMenuGridR   onClick={handleNav}  size={50} />
+            <li className=' cursor-pointer dark:text-white pl-8'><BsMoonFill onClick={()=> setDarkmode(!darkmode)} size={30}/></li>
+          </div>
         </div>
+      </div>
+
+      {/* Mobile Menu */}
+      {/* Overlay */}
+      <div
+        className={
+          nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70' : ''
+        }
+      >
+        {/* Side Drawer Menu */}
+        <div
+          className={
+            nav
+              ? ' fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500 dark:bg-gray-900'
+              : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'
+          }
+        >
+          <div>
+            <div className='flex justify-between text-black dark:text-white p-4'>
+              <Link href='/'>
+                <a>
+                  <Image
+                    src={Logo}
+                    width='187'
+                    height='85'
+                    alt='/'
+                  />
+                </a>
+              </Link>
+              <div
+                onClick={handleNav}
+                className=' p-3 cursor-pointer'
+              >
+                <GiCrossedSwords size={40}/>
+              </div>
+            </div>
+            <div className='border-b border-gray-300 my-4'>
+              <p className='dark:text-green-300 text-green-800 text-center font-bold text-xl  sm:p-1 sm:text-2xl  shadow-xl'>
+                Let&#39;s build something 
+              </p>
+            </div>
+          </div>
+          <div className='py-4 flex flex-col'>
+            <ul className='uppercase'>
+              <Link href='/'>
+                <li onClick={() => setNav(false)} className=' px-3 hover:underline text-2xl font-bold py-4 text-green-800 dark:text-white'>
+                  Home
+                </li>
+              </Link>
+              <Link href='/#about'>
+                <li onClick={() => setNav(false)} className=' px-3 hover:underline text-2xl font-bold py-4 text-green-800 dark:text-white'>
+                  About
+                </li>
+              </Link>
+              <Link href='/#skills'>
+                <li onClick={() => setNav(false)} className=' px-3 hover:underline text-2xl font-bold py-4 text-green-800 dark:text-white'>
+                  Skills
+                </li>
+              </Link>
+              <Link href='/#projects'>
+                <li onClick={() => setNav(false)} className=' px-3 hover:underline text-2xl font-bold py-4 text-green-800 dark:text-white'>
+                  Projects
+                </li>
+              </Link>
+              
+              <Link href='/#contact'>
+                <li onClick={() => setNav(false)} className=' px-3 hover:underline text-2xl font-bold py-4 text-green-800 dark:text-white'>
+                  Contact
+                </li>
+              </Link>
+            </ul>
+            <div className='pt-2'>
+              <p className=' text-green-900 text-bolder text-2xl text-center'>
+                Let&#39;s Connect
+              </p>
+              <div className='flex items-center justify-between my-4 w-full sm:w-[80%] '>
+                <a
+                  href='https://www.linkedin.com/in/clint-briley-50056920a/'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  <div className='rounded-full shadow-lg dark:text-white shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                    <BsLinkedin />
+                  </div>
+                </a>
+                <a
+                  href='https://github.com/fireclint'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  <div className='rounded-full shadow-lg dark:text-white shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                    <BsGithub />
+                  </div>
+                </a>
+                <Link href='/#contact'>
+                  <div
+                    onClick={() => setNav(!nav)}
+                    className='rounded-full shadow-lg shadow-gray-400 dark:text-white p-3 cursor-pointer hover:scale-105 ease-in duration-300'
+                  >
+                    <BsTwitter />
+                  </div>
+                </Link>
+                <Link href='/resume'>
+                  <div
+                    onClick={() => setNav(!nav)}
+                    className='rounded-full shadow-lg shadow-gray-400 dark:text-white p-3 cursor-pointer hover:scale-105 ease-in duration-300'
+                  >
+                    <SiGmail />
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </div>
         </div>
-        {/* OverLay */}
-        <div className={sideBar ? 'md:hidden fixed left-0 top-0 w-full h-full bg-black/70': 'hidden'}>
-
-<div className= {sideBar ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500 dark:bg-gray-900' : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'}>
-<div>
-  <div className='flex justify-between text-black dark:text-white p-4'>
-    <Image src={Logo} width='196' height='72' />
-    <GiCrossedSwords onClick={handleSideBar}  size={40} />
-
-  </div>
-  <p className='dark:text-green-300 text-green-800 text-center font-bold text-xl  sm:p-3 sm:text-3xl  shadow-xl'>Front End Web Dev</p>
-  <ul className='pt-8'>
-  <Link href='/'>
-              <li className=' px-3 hover:underline text-2xl font-bold py-4 text-green-800 dark:text-white'>
-                Home
-              </li>
-            </Link>
-            <Link href='/'>
-              <li className=' px-3 hover:underline text-2xl font-bold py-4 text-green-800 dark:text-white'>
-                Skills
-              </li>
-            </Link>
-            <Link href='/'>
-              <li className=' px-3 hover:underline  text-2xl font-bold py-4 text-green-800 dark:text-white'>
-                Projects
-              </li>
-            </Link>
-            <Link href='/'>
-              <li className=' px-3 hover:underline text-2xl font-bold py-4 text-green-800 dark:text-white'>
-                Experience
-              </li>
-            </Link>
-            <Link href='/'>
-              <li className=' px-3 hover:underline text-2xl font-bold py-4 text-green-800 dark:text-white'>
-                Contact
-              </li>
-            </Link>
-            
-  </ul>
-</div>
-<div className=' pt-4'>
-  <p className='text-center text-2xl text-green-800 font-bold p-2 lg:p-4'>
-    Let's Connect 
-    </p>
-    <div className='flex justify-around'>
-      <div className='rounded-full shadow-lg shadow-gray-400 hover:scale-105  p-3 ease-in duration-300'>
-      <BsLinkedin size={25} />
       </div>
-      <div className='rounded-full shadow-lg shadow-gray-400 hover:scale-105  p-3 ease-in duration-300'>
- <BsGithub />
-      </div>
-      <div className='rounded-full shadow-lg shadow-gray-400 hover:scale-105  p-3 ease-in duration-300'>
-      <BsTwitter />
-      </div>
-      <div className='rounded-full shadow-lg shadow-gray-400 hover:scale-105  p-3 ease-in duration-300'>
-      <SiGmail />
-      </div>
-
     </div>
-</div>
-</div>
-        </div>
-    </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
