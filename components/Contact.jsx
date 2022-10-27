2bef63f577e99557edc69e73fb47d130d8c89a7f
Abhishek <abhishekparmar162@gmail.com>
2022-10-27 23:50:48 +0530

    fix contact error

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -8,9 +8,9 @@ const Contact = () => {
                 <p className='text-4xl font-bold inline border-b-4 border-blue-600 text-green-800  dark:text-gray-300'>Contact</p>
                 <p className='dark:text-gray-300 text-green-800 py-4 pt-4'> Submit the form below or mail me an myemail - iamdevabhi@gmail.com. </p>
             </div>
-            <input className='dark:bg-[#ccd6f6] bg-blue-800  text-gray-300  p-2' type="text" placeholder='Name' name='name' />
-            <input className='my-4 p-2 dark:bg-[#ccd6f6] bg-blue-800 text-gray-300' type="email" placeholder='Email' name='email' />
-            <textarea className='dark:placeholder:bg-[#ccd6f6] p-2 bg-blue-800 w-full text-gray-300' name="message " rows="10" placeholder='Message'></textarea>
+            <input className=' bg-blue-800  text-gray-300  p-2' type="text" placeholder='Name' name='name' />
+            <input className='my-4 p-2  bg-blue-800 text-gray-300' type="email" placeholder='Email' name='email' />
+            <textarea className=' p-2 bg-blue-800 w-full  text-gray-300' name="message " rows="10" placeholder='Message'></textarea>
             <button className='text-white border-2 hover:bg-blue-600 hover:border-blue-600 px-4 py-3 my-8 mx-auto flex items-center'> Collaborate</button>
         </form>
     </div>
