0a4de63f7d6b90c307e78dc739b46b5aa43603b7
Abhishek <abhishekparmar162@gmail.com>
2023-12-07 14:44:51 +0530

    dropbox project added

--- components/ProjectList.jsx
+++ components/ProjectList.jsx
@@ -1,99 +1,100 @@
-import Shop from '../public/assets/shop.jpg'
-import quote from '../public/assets/quote.jpg';
-import image from '../public/assets/imagesearch.jpg';
-import video from '../public/assets/videosearch.jpg'
-import cab from '../public/assets/cab.png'
-import foodieapp from '../public/assets/foodie.jpg'
-import blog from '../public/assets/blog.jpg'
-import chat from '../public/assets/Chat.png' 
+import Shop from "../public/assets/shop.jpg";
+import quote from "../public/assets/quote.jpg";
+import image from "../public/assets/imagesearch.jpg";
+import video from "../public/assets/videosearch.jpg";
+import cab from "../public/assets/cab.png";
+import dropbox from "../public/assets/dropbox.png";
+import foodieapp from "../public/assets/foodie.jpg";
+import blog from "../public/assets/blog.jpg";
+import chat from "../public/assets/Chat.png";
 // import yt from '../public/assets/youtubeV2.png'
-import uber from '../public/assets/uber.png'
-import Todo from '../public/assets/Todo.png'
+import uber from "../public/assets/uber.png";
+import Todo from "../public/assets/Todo.png";
 
 export const ProjectList = [
-    {
-        title: 'NEXTJS BLOG',
-        backgroundImg: cab,
-        skill: 'NEXTJS , Google FIREBASE,TailwindCSS ',
-        source: 'https://github.com/kunwarabhi7/blogNextJS',
-        projectUrl : 'https://meabhishekblog.vercel.app/'
+  {
+    title: "Dropbox CLONE",
+    backgroundImg: dropbox,
+    skill: "NEXT14 , Google FIREBASE,TailwindCSS ,shadcn,Clerk ",
+    source: "https://github.com/kunwarabhi7/dropbox",
+    projectUrl: "https://dropboxclonee.vercel.app/",
+  },
+  {
+    title: "NEXTJS BLOG",
+    backgroundImg: cab,
+    skill: "NEXTJS , Google FIREBASE,TailwindCSS ",
+    source: "https://github.com/kunwarabhi7/blogNextJS",
+    projectUrl: "https://meabhishekblog.vercel.app/",
+  },
+  {
+    title: "NEXTJS CHATAPP",
+    backgroundImg: chat,
+    skill: "NEXTJS , Google FIREBASE,TailwindCSS ",
+    source: "https://github.com/kunwarabhi7/chatAppNextJS",
+    projectUrl: "http://abhimessage.vercel.app/",
+  },
+  {
+    title: "ECommerce Website",
+    backgroundImg: Shop,
+    skill: "React.js , Google FIREBASE , CSS ",
+    source: "https://github.com/kunwarabhi7/shop",
+    projectUrl: "https://shop-devabhi7.vercel.app/",
+  },
+  {
+    title: "Random Quote Generator",
+    backgroundImg: quote,
+    skill: "HTML,CSS,JavaScript",
+    source: "https://github.com/kunwarabhi7/quoteGenerator",
+    projectUrl: "https://abhiquotegenerator.vercel.app/",
+  },
+  {
+    title: "Video Search App",
+    backgroundImg: video,
+    skill: "React.js,YouTubeAPI",
+    source: "https://github.com/kunwarabhi7/youtube",
+    projectUrl: "https://youtube-abhishek.vercel.app/",
+  },
+  {
+    title: "Image Search App",
+    backgroundImg: image,
+    skill: "React.js,unSplashAPI",
+    source: "https://github.com/kunwarabhi7/imagesearch",
+    projectUrl: "https://pics-abhishek.vercel.app/",
+  },
 
-    },
-    {
-        title: 'NEXTJS CHATAPP',
-        backgroundImg: chat,
-        skill: 'NEXTJS , Google FIREBASE,TailwindCSS ',
-        source: 'https://github.com/kunwarabhi7/chatAppNextJS',
-        projectUrl : 'http://abhimessage.vercel.app/'
-
-    },
-    {
-        title: 'ECommerce Website',
-        backgroundImg: Shop,
-        skill: 'React.js , Google FIREBASE , CSS ',
-        source: 'https://github.com/kunwarabhi7/shop',
-        projectUrl : 'https://shop-devabhi7.vercel.app/'
-
-    },
-    {
-        title: 'Random Quote Generator',
-        backgroundImg: quote,
-        skill: 'HTML,CSS,JavaScript',
-        source: 'https://github.com/kunwarabhi7/quoteGenerator',
-        projectUrl: 'https://abhiquotegenerator.vercel.app/'
-    },
-    {
-        title: 'Video Search App',
-        backgroundImg: video,
-        skill: 'React.js,YouTubeAPI',
-        source: 'https://github.com/kunwarabhi7/youtube',
-        projectUrl: 'https://youtube-abhishek.vercel.app/'
-
-
-    },
-    {
-        title: 'Image Search App',
-        backgroundImg: image,
-        skill: 'React.js,unSplashAPI',
-        source: 'https://github.com/kunwarabhi7/imagesearch',
-        projectUrl: 'https://pics-abhishek.vercel.app/'
-    },
-
-    {
-        title: 'Restaurant App',
-        backgroundImg: foodieapp,
-        skill: 'React.js, FIREBASE,CSS ',
-        source: 'https://github.com/kunwarabhi7/resturentapp/',
-        projectUrl: "https://myfoodieapp.vercel.app/"
-    },
-    {
-        title: 'Blog App',
-        backgroundImg: blog,
-        skill: 'React.js, FIREBASE,CSS ',
-        source: 'https://github.com/kunwarabhi7/blog',
-        projectUrl: "https://abhi7blog.vercel.app/"
-    },
-    {
-        title: 'Uber Eats Clone',
-        backgroundImg: uber,
-        skill: 'React.js, TailwindCSS ',
-        source: 'https://github.com/kunwarabhi7/ubereat',
-        projectUrl: "https://ubereat.vercel.app/"
-    },
-    {
-        title: 'Todo App',
-        backgroundImg: Todo,
-        skill: 'NEXTJS, TailwindCSS,Google Firebase ',
-        source: 'https://github.com/kunwarabhi7/todoNextjs',
-        projectUrl: "https://abhitodo.vercel.app/"
-    },
-    // {
-    //     title: 'YouTube Clone App',
-    //     backgroundImg: yt2,
-    //     skill: 'NextJS, RapidAPI,TailwindCSS ',
-    //     source: 'https://github.com/kunwarabhi7/YouTubeV2',
-    //     projectUrl: "https://youtubev2.vercel.app/"
-    // },
-
-
-]
+  {
+    title: "Restaurant App",
+    backgroundImg: foodieapp,
+    skill: "React.js, FIREBASE,CSS ",
+    source: "https://github.com/kunwarabhi7/resturentapp/",
+    projectUrl: "https://myfoodieapp.vercel.app/",
+  },
+  {
+    title: "Blog App",
+    backgroundImg: blog,
+    skill: "React.js, FIREBASE,CSS ",
+    source: "https://github.com/kunwarabhi7/blog",
+    projectUrl: "https://abhi7blog.vercel.app/",
+  },
+  {
+    title: "Uber Eats Clone",
+    backgroundImg: uber,
+    skill: "React.js, TailwindCSS ",
+    source: "https://github.com/kunwarabhi7/ubereat",
+    projectUrl: "https://ubereat.vercel.app/",
+  },
+  {
+    title: "Todo App",
+    backgroundImg: Todo,
+    skill: "NEXTJS, TailwindCSS,Google Firebase ",
+    source: "https://github.com/kunwarabhi7/todoNextjs",
+    projectUrl: "https://abhitodo.vercel.app/",
+  },
+  // {
+  //     title: 'YouTube Clone App',
+  //     backgroundImg: yt2,
+  //     skill: 'NextJS, RapidAPI,TailwindCSS ',
+  //     source: 'https://github.com/kunwarabhi7/YouTubeV2',
+  //     projectUrl: "https://youtubev2.vercel.app/"
+  // },
+];
